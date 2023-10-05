9f65eb1ee1632147541fb0f55d8d8a99c58dbdc3
Jordi Castro <jordi.castro003@gmail.com>
2023-10-05 12:02:56 -0500

    inital skeleton

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Bullet(props) // child of List. List props passed down to each bullet points of the list
+{
+  const { name, done, handleClick } = props;
+  
+  return (
+    <div>
+      style={{}}
+      onClick={handleClick}
+    </div>
+  );
+}
+
+
+class List extends React.Component
+{
+  
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      items: [
+        { name: 'Task 1', done: false},
+        { name: 'Task 2', done: false},
+
+      ],
+
+    };
+  }
+
+  
+  handleClick(i) // handle the click 
+  {
+    // const items =
+  }
+
+
+  render()
+  {
+    return (
+      <div className="list">
+
+
+
+      </div>
+    ); // returns a JSX (JavaScript XML) represents the UI of the component <div>: JSX element structures the content of the component
+  }
+}
+
+// -----------------------------------------
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<List />);
