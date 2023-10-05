a170c336b546e4513666a9ab03735430e685b9aa
Jordi Castro <jordi.castro003@gmail.com>
2023-10-05 16:12:52 -0500

    add task functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-// function Box(props) // possibly a box that clicks and triggers List's done := false.
-// {
+function Button( {onClick} ) // button that  adds a new task
+{
+    const [input, setInput] = useState('');
 
-// }
+    const handleButtonClick = () =>
+    {
+        onClick(input);
+        setInput('');
+    };
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Enter a task"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+
+            <button
+                className="button"
+                onClick={handleButtonClick}
+            >
+            Add Task
+            </button>
+        </div>
+    );
+}
 
 function Bullet(props) // child of List. List props passed down to each bullet points of the list
 {
@@ -40,12 +63,20 @@ class List extends React.Component
   }
 
   
-  handleClick(i) // handle the click 
+  handleClick = (i) => // handle the click 
   {
     const items = this.state.items.slice(); // get the state of the prop: copies the array to ensure immutability
     items[i].done = !items[i].done; // when clicked, change the done logic
     this.setState({items}); // change the state of items and re render!
-  }
+  };
+
+
+  createBullet = (name) =>
+  {
+    const newItem = {name, done:false};
+    this.setState({items: [...this.state.items, newItem]}); // ... creates shallow copy -> ensures immutability
+  };
+  
 
 
   render()
@@ -53,6 +84,7 @@ class List extends React.Component
     return (
         <div className="list">
             <h2>Jordi's To-Do List</h2>
+            <Button onClick={this.createBullet} />
           {this.state.items.map((item, i) => (
             <Bullet
               key={i}
