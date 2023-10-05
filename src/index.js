789fb8e83958481a5a5b19aafbc78c400f6a967f
Jordi Castro <jordi.castro003@gmail.com>
2023-10-05 14:19:27 -0500

    basic cross off logic

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
+// function Box(props) // possibly a box that clicks and triggers List's done := false.
+// {
+
+// }
+
 function Bullet(props) // child of List. List props passed down to each bullet points of the list
 {
   const { name, done, handleClick } = props;
   
   return (
-    <div>
-      style={{}}
+    <div
+      style={{textDecoration: done ? 'line-through' : 'none', color: done ? 'grey' : 'black'}}
       onClick={handleClick}
+    >
+        {name}
     </div>
-  );
+  ); // strike through and grey out if done, else none. call List's handleClick to find logic
 }
 
 
@@ -29,25 +36,33 @@ class List extends React.Component
 
       ],
 
-    };
+    }; // state of the list contains the tasks and their done logic
   }
 
   
   handleClick(i) // handle the click 
   {
-    // const items =
+    const items = this.state.items.slice(); // get the state of the prop: copies the array to ensure immutability
+    items[i].done = !items[i].done; // when clicked, change the done logic
+    this.setState({items}); // change the state of items and re render!
   }
 
 
   render()
   {
     return (
-      <div className="list">
-
-
-
-      </div>
-    ); // returns a JSX (JavaScript XML) represents the UI of the component <div>: JSX element structures the content of the component
+        <div className="list">
+            <h2>Jordi's To-Do List</h2>
+          {this.state.items.map((item, i) => (
+            <Bullet
+              key={i}
+              name={item.name}
+              done={item.done}
+              handleClick={() => this.handleClick(i)}
+            />
+          ))}
+        </div>
+      ); // returns a JSX (JavaScript XML) represents the UI of the component <div>: JSX element structures the content of the component
   }
 }
 
